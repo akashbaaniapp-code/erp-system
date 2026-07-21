44116af7be1677e0d1b7697a6b072644694860be
akashbaaniapp-code <akashbaaniapp@gmail.com>
2026-07-22 01:44:01 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-export const metadata = {
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
   title: "ERP System",
-  description: "ERP System",
+  description: "Business ERP Management System",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
- 
