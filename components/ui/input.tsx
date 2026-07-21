0f59803c4fdfe86282aadcd1c0c72b42145a68f6
akashbaaniapp-code <akashbaaniapp@gmail.com>
2026-07-22 01:03:50 +0600

    Add Input component

---
+++ components/ui/input.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm outline-none focus:border-black ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
