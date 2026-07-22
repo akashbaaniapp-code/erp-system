81a22ac9a7fcc174efa88c72cad91381ed2fd474
akashbaaniapp-code <akashbaaniapp@gmail.com>
2026-07-22 13:09:43 +0600

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "@/components/layout/dashboard-layout"
-
-export default function ProductsLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>
-}
