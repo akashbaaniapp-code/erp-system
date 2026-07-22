d05835e83a8f0282a19d882c65a96483c8724d04
akashbaaniapp-code <akashbaaniapp@gmail.com>
2026-07-22 12:19:26 +0600

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
 const globalForPrisma = globalThis as {
   prisma?: PrismaClient;
 };
 
+const client = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(client);
+
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
